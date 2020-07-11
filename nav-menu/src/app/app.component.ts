89b409a6e68a982d7554ff418b6cfaf41c69f902
Koiwu Beyan <Koiwu.Beyan@blackrock.com>
2020-07-10 23:07:30 -0700

    Create standard JSON data file; Add NGRX/Store to package.json; Add HTTPClient to retrieve JSON local JSON data; ToDo: move JSON data to service

--- nav-menu/src/app/app.component.ts
+++ nav-menu/src/app/app.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
 export class AppComponent {
+  public data: any;
+  constructor(private http: HttpClient){
+    this.data = this.http.get('../assets/data/data.json');
+    this.data.subscribe((res) => {
+      console.log(res);
+    });
+  }
   title = 'nav-menu';
 }
